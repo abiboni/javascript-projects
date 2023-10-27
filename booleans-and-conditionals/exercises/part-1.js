988d2657241616a58bda680840aac59454388885
abiboni <abigail.o.bonilla@gmail.com>
2023-10-27 11:36:56 -0400

    Crew Status

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -4,7 +4,7 @@ let spaceSuitsOn = true;
 let shuttleCabinReady = true;
 let crewStatus = spaceSuitsOn && shuttleCabinReady;
 let computerStatusCode = 200;
-let shuttleSpeed = 15000
+let shuttleSpeed = 15000;
 
 // BEFORE running the code, predict what will be printed to the console by the following statements:
 
@@ -14,4 +14,9 @@ if (engineIndicatorLight === "green") {
    console.log("engines are preparing to start");
 } else {
    console.log("engines are off");
+}
+if (crewStatus === true) {
+   console.log("Crew Ready");
+} else {
+   console.log("Crew Not Ready");
 }
